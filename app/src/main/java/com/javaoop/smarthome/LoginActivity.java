91c77a903493e708b5be66c4ac68f39bea92065f
ThanhHai24 <161220433+ThanhHai24@users.noreply.github.com>
2024-11-14 06:08:09 +0700

    Thêm hoạt động nút ở Login + Register Sửa giao diện login

--- app/src/main/java/com/javaoop/smarthome/LoginActivity.java
+++ app/src/main/java/com/javaoop/smarthome/LoginActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -11,7 +14,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
-
+    EditText username;
+    EditText password;
+    Button loginButton;
 
 
     @Override
@@ -19,6 +24,26 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
+        username = findViewById(R.id.username_input);
+        password = findViewById(R.id.password_input);
+        loginButton = findViewById(R.id.login_btn);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+                    Toast.makeText(LoginActivity.this, "Đăng nhập thành công.", Toast.LENGTH_SHORT).show();
+                    //Intent forgot = new Intent(LoginActivity.this , ForgotActivity.class);
+                    //startActivity(forgot);
+                }else if(username.getText().toString().equals("") || password.getText().toString().equals("")) {
+                    Toast.makeText(LoginActivity.this, "Vui lòng nhập đầy đủ tài khoản và mật khẩu.", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(LoginActivity.this, "Sai tài khoản hoặc mật khẩu.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login_screen), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/javaoop/smarthome/RegisterActivity.java
+++ app/src/main/java/com/javaoop/smarthome/RegisterActivity.java
@@ -3,6 +3,9 @@ package com.javaoop.smarthome;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,12 +14,34 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class RegisterActivity extends AppCompatActivity {
+    EditText newemail;
+    EditText password;
+    EditText comfirmpassword;
+    Button registerButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
+
+        newemail = findViewById(R.id.new_email_input);
+        password = findViewById(R.id.new_pass_input);
+        comfirmpassword = findViewById(R.id.confirm_pass);
+        registerButton = findViewById(R.id.register_btn);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(newemail.getText().toString().isEmpty() || password.getText().toString().isEmpty() || comfirmpassword.getText().toString().isEmpty()){
+                    Toast.makeText(RegisterActivity.this, "Vui lòng nhập đầy đủ thông tin.", Toast.LENGTH_SHORT).show();
+                } else if (!comfirmpassword.getText().toString().equals(password.getText().toString())) {
+                    Toast.makeText(RegisterActivity.this, "Mật khẩu chưa trùng khớp.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.register_screen), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
